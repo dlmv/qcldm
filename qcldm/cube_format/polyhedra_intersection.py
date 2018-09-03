9a9eee6086081788ef7bf2c8caeb351a23540684
dlmv <danya.malcev@gmail.com>
2018-09-03 22:25:46 +0300

    cuboid intersection

--- qcldm/cube_format/polyhedra_intersection.py
+++ qcldm/cube_format/polyhedra_intersection.py
@@ -2,12 +2,13 @@ import re, os, sys, math, logging
 from math3d import Vector
 import numpy as np
 from scipy.spatial import Delaunay
+import itertools
 
 def tetrahedron_volume(a, b, c, d):
 	return np.abs(np.einsum('ij,ij->i', a-d, np.cross(b-d, c-d))) / 6
 	
 def hull_volume(pts):
-	if len(pts) < 4:
+	if not validate_points(pts):
 		return 0
 	pts = [np.array([p.x, p.y, p.z]) for p in pts]
 	dt = Delaunay(pts)
@@ -15,6 +16,22 @@ def hull_volume(pts):
 	vol = np.sum(tetrahedron_volume(tets[:, 0], tets[:, 1], 
                                 tets[:, 2], tets[:, 3]))
 	return vol
+
+def validate_points(pts):
+	if len(pts) < 4:
+		return False
+	for i in range(len(pts) - 3):
+		for i1 in range(len(pts) - 2):
+			for i2 in range(len(pts) - 1):
+				for i3 in range(len(pts)):
+					v1 = pts[i1] - pts[i]
+					v2 = pts[i2] - pts[i]
+					v3 = pts[i3] - pts[i]
+					if (v1 * (v2.cross(v3))) != 0:
+						return True
+	return False
+						
+					
 
 class Plane:
 	def __init__(self, n, d):
@@ -34,6 +51,9 @@ def plane_3points(p1, p2, p3):
 	d = - cp * p3
 	return Plane(cp, d)
 
+def plane_2vectors(o, v1, v2):
+	return plane_3points(o, o + v1, o + v2)
+
 class CuttingPlane:
 	def __init__(self, plane, inner):
 		self.plane = plane
@@ -46,7 +66,6 @@ def plane3_intersection(p1, p2, p3):
 	return -(p1.d * (p2.n.cross(p3.n)) + p2.d * (p3.n.cross(p1.n)) + p3.d * (p1.n.cross(p2.n))) / div
 
 def same_side(p1, p2, plane):
-	print  plane.distance(p1), plane.distance(p2)
 	return plane.distance(p1) * plane.distance(p2) >= 0
 
 class PlaneCut:
@@ -57,7 +76,6 @@ class PlaneCut:
 	def add(self, plane):
 		new_vertices = []
 		for v in self.vertices:
-			print v
 			ok = True
 			if not same_side(v, plane.inner, plane.plane):
 				ok = False
@@ -81,6 +99,39 @@ class PlaneCut:
 					if ok:
 						self.vertices.append(p)
 		self.planes.append(plane)
+
+class Cuboid:
+	def __init__(self, origin, vectors):
+		self.origin = origin
+		self.vectors = vectors
+
+	def center(self):
+		return self.origin + reduce(lambda a,b: a+b, self.vectors) / 2
+
+	def cutting_planes(self):
+		planes = []
+		center = self.center()
+		end = self.origin + reduce(lambda a,b: a+b, self.vectors)
+		planes.append(CuttingPlane(plane_2vectors(self.origin, self.vectors[0], self.vectors[1]), center))
+		planes.append(CuttingPlane(plane_2vectors(self.origin, self.vectors[1], self.vectors[2]), center))
+		planes.append(CuttingPlane(plane_2vectors(self.origin, self.vectors[2], self.vectors[0]), center))
+		planes.append(CuttingPlane(plane_2vectors(end, self.vectors[0], self.vectors[1]), center))
+		planes.append(CuttingPlane(plane_2vectors(end, self.vectors[1], self.vectors[2]), center))
+		planes.append(CuttingPlane(plane_2vectors(end, self.vectors[2], self.vectors[0]), center))
+		return planes
+
+def cuboid_intersection(c1, c2):
+	pc = PlaneCut()
+	for cp in c1.cutting_planes() + c2.cutting_planes():
+		pc.add(cp)
+	return hull_volume(pc.vertices)
+
+
+
+
+
+
+
 
 
 
