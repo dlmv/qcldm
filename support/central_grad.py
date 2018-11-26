d3c44799d320b038b54ddbdf97ad0a2ac3d904a4
dlmv <danya.malcev@gmail.com>
2018-11-26 22:39:18 +0300

    support scripts

---
+++ support/central_grad.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python 
+import os, sys
+
+def read_embed_positions():
+	pos = []
+	nepos = []
+	with open("coord") as c:
+		n = 0
+		for l in c:
+			ls = l.split()
+			if '$' not in l and ls:
+				if ls[-1] == 'zz':
+					pos += [n]
+				elif ls[-1] == 'ne':#FIXME
+					nepos += [n]
+				n += 1
+	return n, pos, nepos
+
+def read_grad_from_control(num, pos, nepos):
+	with open("control") as c:
+		lines = c.read().splitlines()
+		n = 0
+		while n < len(lines):
+			if '$grad' in lines[n]:
+				break
+			n += 1
+		assert n < len(lines)
+		grad = 0.
+		while n < len(lines):
+			while n < len(lines):
+				if lines[n][0] == '#':
+					n += 1
+					continue
+				if 'cycle' in lines[n]:
+					break
+				n += 1
+			if n == len(lines):
+				return grad
+			grad = 0.
+			rawgrads = []
+			n0 = n
+			for i in range(num):
+				n = n0 + num + 1 +  i
+				igrads = [float(l.replace("D", "E")) for l in lines[n].split()]
+				if i not in pos + nepos:
+					rawgrads.extend(igrads)
+			grad = (reduce(lambda s, i: s + i**2, rawgrads, 0.))**0.5
+
+
+n, pos, nepos = read_embed_positions()
+print read_grad_from_control(n, pos, nepos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
