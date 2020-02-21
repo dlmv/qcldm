d3382a17b66cbac85601a61529b8bcfec0d8e7bc
dlmv <danya.malcev@gmail.com>
2020-02-21 21:17:15 +0300

    fixes and improvements

--- qcldm/applications/turbo_cell_opt.py
+++ qcldm/applications/turbo_cell_opt.py
@@ -64,50 +64,74 @@ class OptData:
 		assert os.system('part_dscf > log_dscf.log') == 0
 		time.sleep(1)
 
-	def grad_step(self, vector_step):
+	def grad_step(self):
 		assert os.system('part_grad > log_grad.log') == 0
 		time.sleep(1)
+
+	def read_grads(self):
 		x = read_grad(len(self.atoms))
 		xr = self.real_x(x)
-		real_grad = (reduce(lambda s, i: s + i**2, xr, 0.))**0.5
+		xv1, _ = self.co.cell.cut_and_expand_by_vectors(xr, self.real_atoms(), self.atoms)
+		xc1, _ = self.co.cell.cut_and_expand_by_coords(xr, self.real_atoms(), self.atoms)
+		xv1r = self.real_x(xv1)
+		xc1r = self.real_x(xc1)
+		grad = (reduce(lambda s, i: s + i**2, xr, 0.))**0.5
+		grad_v = (reduce(lambda s, i: s + i**2, xv1r, 0.))**0.5
+		grad_c = (reduce(lambda s, i: s + i**2, xc1r, 0.))**0.5	
+		return grad, grad_v, grad_c	
+
+	def rewrite_grad(self, vector_step):
+		x = read_grad(len(self.atoms))
+		xr = self.real_x(x)
 		x1, _ = self.co.cell.cut_and_expand_by_vectors(xr, self.real_atoms(), self.atoms) if vector_step else self.co.cell.cut_and_expand_by_coords(xr, self.real_atoms(), self.atoms)
 		write_grad(self.cleared_x(x1))
-		x1r = self.real_x(x1)
-		grad = (reduce(lambda s, i: s + i**2, x1r, 0.))**0.5
-		return grad, real_grad
 
-	def relax_step(self, damp, vector_step):
+	def rewrite_coords(self, vector_step, damp):
 		old_atoms = self.atoms
-		assert os.system('part_relax > log_relax.log') == 0
-		time.sleep(1)
 		new_atoms = read_atoms('coord')
 		dx = self.coord_delta(old_atoms, new_atoms, damp)
 		dxr = self.real_x(dx)
 		dx1, _ = self.co.cell.cut_and_expand_by_vectors(dxr, self.real_atoms(), self.atoms) if vector_step else self.co.cell.cut_and_expand_by_coords(dxr, self.real_atoms(), self.atoms)
 		self.co.cell.modify_by_vectors(dxr, self.real_atoms()) if vector_step else self.co.cell.modify_by_coords(dxr, self.real_atoms())
 		self.apply_delta(dx1)
+
+	def relax_step(self, damp, vector_step):
+		self.rewrite_grad(vector_step)
+		assert os.system('part_relax > log_relax.log') == 0
+		time.sleep(1)
+		self.rewrite_coords(vector_step, damp)
 		write_crystal_part(self.co, 'cell.restart')
 		write_atoms(self.atoms, 'coord.restart')
 		
-	def write_result(self, grad, real_grad, typ, damp):
+	def write_result(self, grad, grad_v, grad_c, vector_step, damp):
+		typ = 'VCTR' if vector_step else 'ICRD'
 		with open("coord.log", "a") as log:
-			log.write("step = %5s | grad = %13.8f | full_grad = %13.8f | damp = %5.3f\n" % (typ, grad, real_grad, damp))
+			log.write("%s | grad = %11.8f | grad_v = %11.8f | grad_c = %11.8f | damp = %4.2f\n" % (typ, grad, grad_v, grad_c, damp))
+
+	def write_result_init(self, grad, grad_v, grad_c, damp):
+		typ = 'INIT'
+		with open("coord.log", "a") as log:
+			log.write("%s | grad = %11.8f | grad_v = %11.8f | grad_c = %11.8f | damp = %4.2f\n" % (typ, grad, grad_v, grad_c, damp))
 		
 	def semistep(self, damp, vector_step):
 		if self.first:
 			self.dscf_step()
-			grad, real_grad = self.grad_step(vector_step)
-			self.write_result(grad, real_grad, 'vector' if vector_step else 'coord', damp)
+			self.grad_step()
+			grad, grad_v, grad_c = self.read_grads()
+			self.write_result_init(grad, grad_v, grad_c, damp)
 			self.first = False
 		self.relax_step(damp, vector_step)
 		self.dscf_step()
-		grad, real_grad = self.grad_step(vector_step)
-		self.write_result(grad, real_grad, 'vector' if vector_step else 'coord', damp)
-		return grad
+		self.grad_step()
+		grad, grad_v, grad_c = self.read_grads()
+		self.write_result(grad, grad_v, grad_c, vector_step, damp)
+		return grad_v if vector_step else grad_c
 		
 	def step(self, damp):
 		grad_c = self.semistep(damp, False)
+		clear_relax()
 		grad_v = self.semistep(damp, True)
+		clear_relax()
 		return max(grad_c, grad_v)
 		
 	def optimize_alternating(self, eps):
@@ -128,21 +152,23 @@ class OptData:
 			if abs(grad1-grad) < eps:
 				break
 			grad = grad1
+			clear_relax()
 			while True:
 				grad1 = self.semistep(damp, True)
 				if abs(grad1-grad) < eps:
 					break
 				grad = grad1
+			clear_relax()
 			if grad < eps * 10:
 				break
 
 	def optimize(self, eps):
 		with open("coord.log", "w") as log:
 			log.write("OPTIMIZATION START: eps=%.2e\n" % eps)
-		self.optimize_alternating(eps)
-#		self.optimize_longrun(eps)
-		with open("coord.log", "w") as log:
-			log.write("OPTIMIZATION FINISH\n" % eps)
+#		self.optimize_alternating(eps)
+		self.optimize_longrun(eps)
+		with open("coord.log", "a") as log:
+			log.write("OPTIMIZATION FINISH\n")
 
 
 
